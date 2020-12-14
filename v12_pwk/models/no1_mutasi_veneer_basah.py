4ad4566f20191b5157414751237bb5dca9f66555
dionmartin <dion.martin.h@gmail.com>
2020-12-14 08:44:52 +0700

    samuel

--- v12_pwk/models/no1_mutasi_veneer_basah.py
+++ v12_pwk/models/no1_mutasi_veneer_basah.py
@@ -310,7 +310,7 @@ class PwkMutasiVeneerBasah(models.Model):
                         })
 
     @api.multi
-    def button_print(self, workbook, data, lines):
+    def button_print(self, workbook):
         alamat = ' Jl. Raya Krangan - Pringsurat, Karanglo, Kupen, Kec. Pringsurat, Kabupaten Temanggung, Jawa Tengah 56272'
 
         sheet = workbook.add_worksheet('Laporan PEB')
