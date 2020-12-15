189cb1231b7b423fa06a824e64350c0e0fb3d386
dionmartin <dion.martin.h@gmail.com>
2020-12-15 12:24:34 +0700

    samuel

--- v12_pwk/models/no3_mutasi_veneer_klindry.py
+++ v12_pwk/models/no3_mutasi_veneer_klindry.py
@@ -12,6 +12,110 @@ import math
 import re    
 from num2words import num2words
 
+class PwkMutasiVeneerKlinDryReline(models.Model):
+    _name = "pwk.mutasi.veneer.klindry.reline"
+
+    reference = fields.Many2one('pwk.mutasi.veneer.klindry', 'Reference')
+    product_id = fields.Many2one('product.product', 'Product')
+    tebal = fields.Float(compute="_get_product_attribute", string='Tebal')
+    lebar = fields.Float(compute="_get_product_attribute", string='Lebar')
+    panjang = fields.Float(compute="_get_product_attribute", string='Panjang')
+    grade = fields.Many2one(compute="_get_product_attribute", comodel_name='pwk.grade', string='Grade')
+    stock_awal_pcs = fields.Float(compute="_get_stock_awal", string='Stok Awal')
+    stock_awal_vol = fields.Float(compute="_get_volume", string='Stok Awal', digits=dp.get_precision('FourDecimal'))
+    stock_masuk_pcs = fields.Float(compute="_get_stock_masuk", string='Stok Masuk Basah')
+    stock_masuk_vol = fields.Float(compute="_get_volume", string='Stok Masuk Basah', digits=dp.get_precision('FourDecimal'))
+    acc_stock_masuk_pcs = fields.Float(compute="_get_acc", string='Stok Masuk Basah')
+    acc_stock_masuk_vol = fields.Float(compute="_get_volume", string='Stok Masuk Basah', digits=dp.get_precision('FourDecimal'))
+    stock_keluar_pcs = fields.Float('Stok Keluar Kering')
+    stock_keluar_vol = fields.Float(compute="_get_volume", string='Stok Keluar Kering', digits=dp.get_precision('FourDecimal'))
+    acc_stock_keluar_pcs = fields.Float(compute="_get_acc", string='Stok Keluar Kering')
+    acc_stock_keluar_vol = fields.Float(compute="_get_volume", string='Stok Keluar Kering', digits=dp.get_precision('FourDecimal'))
+    stock_akhir_pcs = fields.Float(compute="_get_stock_akhir", string='Stok Akhir')
+    stock_akhir_vol = fields.Float(compute="_get_volume", string='Stok Akhir', digits=dp.get_precision('FourDecimal'))
+
+    @api.depends('product_id')
+    def _get_product_attribute(self):
+        for res in self:
+            if res.product_id:
+                res.tebal = res.product_id.tebal
+                res.lebar = res.product_id.lebar
+                res.panjang = res.product_id.panjang
+                res.grade = res.product_id.grade.id
+
+    @api.depends('stock_awal_pcs','stock_masuk_pcs','stock_keluar_pcs')
+    def _get_volume(self):
+        for res in self:
+            res.stock_awal_vol = res.stock_awal_pcs * res.tebal * res.lebar * res.panjang / 1000000000
+            res.stock_masuk_vol = res.stock_masuk_pcs * res.tebal * res.lebar * res.panjang / 1000000000
+            res.stock_keluar_vol = res.stock_keluar_pcs * res.tebal * res.lebar * res.panjang / 1000000000
+            res.acc_stock_masuk_vol = res.acc_stock_masuk_pcs * res.tebal * res.lebar * res.panjang / 1000000000
+            res.acc_stock_keluar_vol = res.acc_stock_keluar_pcs * res.tebal * res.lebar * res.panjang / 1000000000
+            res.stock_akhir_vol = res.stock_akhir_pcs * res.tebal * res.lebar * res.panjang / 1000000000
+
+    @api.depends('stock_awal_pcs','stock_masuk_pcs','stock_keluar_pcs')
+    def _get_acc(self):
+        for res in self:
+            acc_stock_masuk_pcs = 0
+            acc_stock_keluar_pcs = 0
+
+            source_ids = self.env['pwk.mutasi.veneer.klindry.reline'].search([
+                ('reference.date','=',res.reference.date - timedelta(1)),
+                ('product_id','=',res.product_id.id)
+                ])
+
+            if not source_ids:
+                source_ids = self.env['pwk.mutasi.veneer.klindry.reline'].search([
+                    ('reference.date','<',res.reference.date),
+                    ('product_id','=',res.product_id.id)
+                    ])
+
+            if source_ids:
+                acc_stock_masuk_pcs = source_ids[0].acc_stock_masuk_pcs
+                acc_stock_keluar_pcs = source_ids[0].acc_stock_keluar_pcs
+
+            res.acc_stock_masuk_pcs = acc_stock_masuk_pcs + res.stock_masuk_pcs
+            res.acc_stock_keluar_pcs = acc_stock_keluar_pcs + res.stock_keluar_pcs
+
+    @api.depends('product_id')
+    def _get_stock_awal(self):
+        for res in self:
+            stock_awal_pcs = 0
+            source_ids = self.env['pwk.mutasi.veneer.klindry.reline'].search([
+                ('reference.date','=',res.reference.date - timedelta(1)),
+                ('product_id','=',res.product_id.id)
+                ])
+
+            if not source_ids:
+                source_ids = self.env['pwk.mutasi.veneer.klindry.reline'].search([
+                    ('reference.date','<',res.reference.date),
+                    ('product_id','=',res.product_id.id)
+                    ])
+                        
+            if source_ids:
+                stock_awal_pcs = source_ids[0].stock_akhir_pcs
+
+            res.stock_awal_pcs = stock_awal_pcs
+
+    @api.depends('product_id')
+    def _get_stock_masuk(self):
+        for res in self:
+            stock_masuk_pcs = 0
+            source_ids = self.env['pwk.mutasi.veneer.klindry.line'].search([
+                ('reference.date','=',res.reference.date - timedelta(1)),
+                ('product_id','=',res.product_id.id)
+                ])
+                        
+            if source_ids:
+                stock_masuk_pcs = source_ids[0].stock_keluar_pcs
+
+            res.stock_masuk_pcs = stock_masuk_pcs
+
+    @api.depends('stock_awal_pcs','stock_masuk_pcs','stock_keluar_pcs')
+    def _get_stock_akhir(self):
+        for res in self:
+            res.stock_akhir_pcs = res.stock_awal_pcs + res.stock_masuk_pcs - res.stock_keluar_pcs
+
 class PwkMutasiVeneerKlinDryLine(models.Model):
     _name = "pwk.mutasi.veneer.klindry.line"
 
@@ -101,13 +205,13 @@ class PwkMutasiVeneerKlinDryLine(models.Model):
     def _get_stock_masuk(self):
         for res in self:
             stock_masuk_pcs = 0
-            source_ids = self.env['pwk.mutasi.veneer.basah.stacking'].search([
+            source_ids = self.env['pwk.mutasi.veneer.basah.kd'].search([
                 ('reference.date','=',res.reference.date - timedelta(1)),
                 ('product_id','=',res.product_id.id)
                 ])
                         
             if source_ids:
-                stock_masuk_pcs = source_ids[0].stock_keluar_stacking_pcs
+                stock_masuk_pcs = source_ids[0].stock_keluar_pcs
 
             res.stock_masuk_pcs = stock_masuk_pcs
 
@@ -124,7 +228,8 @@ class PwkMutasiVeneerKlindry(models.Model):
     date = fields.Date('Tanggal', default=fields.Date.today(), track_visibility="always")
     user_id = fields.Many2one('res.users', string="Dibuat Oleh", default=lambda self: self.env.user, track_visibility="always")
     state = fields.Selection([('Draft','Draft'),('Approved','Approved')], string="Status", default="Draft", track_visibility="always")
-    line_ids = fields.One2many('pwk.mutasi.veneer.klindry.line', 'reference', string="RolerDryer", track_visibility="always")
+    line_ids = fields.One2many('pwk.mutasi.veneer.klindry.line', 'reference', string="Klin Dry", track_visibility="always")
+    reline_ids = fields.One2many('pwk.mutasi.veneer.klindry.reline', 'reference', string="Re-KD", track_visibility="always")
 
     def get_sequence(self, name=False, obj=False, context=None):
         sequence_id = self.env['ir.sequence'].search([
@@ -145,18 +250,37 @@ class PwkMutasiVeneerKlindry(models.Model):
     @api.multi
     def button_reload(self):
         for res in self:
-            source_ids = self.env['pwk.mutasi.veneer.basah.stacking'].search([
+            source_ids = self.env['pwk.mutasi.veneer.basah.kd'].search([
                 ('reference.date','=',res.date - timedelta(1)),
                 ])
 
             if not source_ids:
-                source_ids = self.env['pwk.mutasi.veneer.basah.stacking'].search([
+                source_ids = self.env['pwk.mutasi.veneer.basah.kd'].search([
                     ('reference.date','<',res.date),
                     ])
 
             if source_ids:
                 for source in source_ids:
                     self.env['pwk.mutasi.veneer.klindry.line'].create({
+                        'reference': res.id,
+                        'product_id': source.product_id.id,
+                        })
+
+    @api.multi
+    def button_reload_re(self):
+        for res in self:
+            source_ids = self.env['pwk.mutasi.veneer.klindry.line'].search([
+                ('reference.date','=',res.date - timedelta(1)),
+                ])
+
+            if not source_ids:
+                source_ids = self.env['pwk.mutasi.veneer.klindry.line'].search([
+                    ('reference.date','<',res.date),
+                    ])
+
+            if source_ids:
+                for source in source_ids:
+                    self.env['pwk.mutasi.veneer.klindry.reline'].create({
                         'reference': res.id,
                         'product_id': source.product_id.id,
                         })
