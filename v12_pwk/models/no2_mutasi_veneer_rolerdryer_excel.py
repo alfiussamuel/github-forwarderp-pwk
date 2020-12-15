cde07f5a2aab617c4536f681b3fc097f6bdd2226
dionmartin <dion.martin.h@gmail.com>
2020-12-15 12:06:31 +0700

    samuel

--- v12_pwk/models/no2_mutasi_veneer_rolerdryer_excel.py
+++ v12_pwk/models/no2_mutasi_veneer_rolerdryer_excel.py
@@ -221,6 +221,8 @@ class MutasiVeneerRolerReportXls(models.AbstractModel):
             row += 1
             number += 1
 
+        row += 2
+
         # Footer
         sheet.merge_range(row+2, 17, row+2, 19, 'Temanggung, ' + lines.date.strftime("%d-%m-%Y"), formatHeaderTable)
         sheet.merge_range(row+3, 1, row+3, 6, 'Mengetahui,', formatHeaderTable)
