d3bc7c98d48207369419b2437ad4932096dc46f6
dionmartin <dion.martin.h@gmail.com>
2020-12-16 07:47:26 +0700

    samuel

--- v12_pwk/models/pwk.py
+++ v12_pwk/models/pwk.py
@@ -13,6 +13,12 @@ import math
 import re    
 from num2words import num2words
 
+class SaleOrderLine(models.Model):    
+    _inherit = "sale.order.line"
+
+    sale_date_order = fields.Date(related='order_id.date_order', string='Date Order')
+    sale_partner_id = fields.Date(related='order_id.partner_id', comodel_name='res.partner', string='Customer')
+
 class PwkRpbLineContainer(models.Model):    
     _name = "pwk.rpb.line.container"
 
--- v12_pwk/wizard/generate_rpb.py
+++ v12_pwk/wizard/generate_rpb.py
@@ -9,8 +9,8 @@ from odoo import models, fields, api
 class PwkGenerateRpbWizard(models.TransientModel):
     _name = 'pwk.generate.rpb.wizard'
 
-    sale_ids = fields.Many2many('sale.order', 'rpb_sale_default_rel',
-        'rpb_id', 'sale_id', string='Sales Orders')
+    sale_line_ids = fields.Many2many('sale.order.line', 'rpb_sale_line_default_rel',
+        'rpb_id', 'sale_line_id', string='Sales Orders')
 
     @api.multi
     def button_generate(self):    	
@@ -19,22 +19,11 @@ class PwkGenerateRpbWizard(models.TransientModel):
     	rpb_id = self.env['pwk.rpb'].search([('id', '=', active_id)])
 
     	if self.sale_ids:
-    		for sale in self.sale_ids:
-    			for line in sale.order_line:
-	    			self.env['pwk.rpb.line'].create({
-	    				'reference': rpb_id.id,
-	    				'sale_id': sale.id,
-	    				'sale_line_id': line.id
-	    				})
+    		for line in self.sale_line_ids:
+    			self.env['pwk.rpb.line'].create({
+    				'reference': rpb_id.id,
+    				'sale_id': line.order_id.id,
+    				'sale_line_id': line.id
+    				})
 
     	return True
-        # context = dict(self._context or {})
-        # active_id = context.get('active_id', False)
-        # print ("active ID ", active_id)
-        # context = dict(self._context or {})
-        # print ("Context ", self.env.context)
-        # self.model = self.env.context.get('res_model')
-
-        # docs = self.env[self.model].browse(self.env.context.get('active_id'))
-        # print ("Docs ", docs)
-        # print ("Model ", self.model)
