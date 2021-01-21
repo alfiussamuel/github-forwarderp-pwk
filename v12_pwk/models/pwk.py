6068479e964959e94724f940e7851ec00c118590
dionmartin <dion.martin.h@gmail.com>
2021-01-21 11:36:47 +0700

    samuel

--- v12_pwk/models/pwk.py
+++ v12_pwk/models/pwk.py
@@ -626,10 +626,12 @@ class PwkRpb(models.Model):
 
     @api.multi
     def action_view_lines(self):
-        action = self.env.ref('v12_pwk.pwk_rpb_line_tree').read()[0]
+        action = self.env.ref('v12_pwk.pwk_rpb_line_tree').read()
         if len(self.line_ids) > 1:
+            print ("masuk 1")
             action['domain'] = [('reference', '=', self.id)]
         else:
+            print ("masuk 2")
             action = {'type': 'ir.actions.act_window_close'}
         return action
 
