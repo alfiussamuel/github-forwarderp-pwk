e47634ca1aaefa109b943b47f2bdc4b274ba113d
Alfius Samuel <samuel.alfius@gmail.com>
2021-02-02 10:05:20 +0700

    Add Manual Number and Global Discount Invoice

--- v12_pwk/models/pwk.py
+++ v12_pwk/models/pwk.py
@@ -2286,12 +2286,12 @@ class AccountInvoice(models.Model):
 
     @api.one
     @api.depends('invoice_line_ids.price_subtotal', 'tax_line_ids.amount', 'tax_line_ids.amount_rounding',
-                 'currency_id', 'company_id', 'date_invoice', 'type', 'formula_type')
+                 'currency_id', 'company_id', 'date_invoice', 'type', 'formula_type', 'global_discount')
     def _compute_amount(self):
         round_curr = self.currency_id.round
         self.amount_untaxed = sum(line.price_subtotal for line in self.invoice_line_ids)
         self.amount_tax = sum(round_curr(line.amount_total) for line in self.tax_line_ids)
-        self.amount_total = self.amount_untaxed + self.amount_tax
+        self.amount_total = self.amount_untaxed + self.amount_tax - self.global_discount
         amount_total_company_signed = self.amount_total
         amount_untaxed_signed = self.amount_untaxed
         if self.currency_id and self.company_id and self.currency_id != self.company_id.currency_id:
@@ -2302,6 +2302,9 @@ class AccountInvoice(models.Model):
         self.amount_total_company_signed = amount_total_company_signed * sign
         self.amount_total_signed = self.amount_total * sign
         self.amount_untaxed_signed = amount_untaxed_signed * sign
+
+    global_discount = fields.Float('Discount')
+    manual_number = fields.Char('Manual No.')
 
     is_saldo_awal = fields.Boolean('Saldo Awal')
     original_number = fields.Char('Original No.')
