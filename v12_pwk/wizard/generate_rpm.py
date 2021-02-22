b0c58d70035f89190b699b814038d4919e217250
Alfius Samuel <samuel.alfius@gmail.com>
2021-02-22 11:49:04 +0700

    Update generate_rpm.py

--- v12_pwk/wizard/generate_rpm.py
+++ v12_pwk/wizard/generate_rpm.py
@@ -26,7 +26,7 @@ class PwkGenerateRpmWizard(models.TransientModel):
     	if self.rpb_line_ids:
             rpm_id.write({'rpb_id': self.rpb_id.id})
             for line in self.rpb_line_ids:
-                self.env['pwk.rpm.line'].create({
+                rpm_line_id = self.env['pwk.rpm.line'].create({
                     'reference': rpm_id.id,
                     'rpb_line_id': line.id,
                     'rpb_id': line.reference.id,
@@ -34,3 +34,34 @@ class PwkGenerateRpmWizard(models.TransientModel):
                     'sale_id': line.sale_line_id.order_id.id,
                     'total_qty': line.subtotal_qty
                 })
+
+            if rpm_line_id:
+                if rpm_line_id.is_detail1:
+                    model_detail = self.env['pwk.rpm.line.detail1']
+                    list_detail = rpm_line_id.detail_ids_1
+                elif rpm_line_id.is_detail2:
+                    model_detail = self.env['pwk.rpm.line.detail2']
+                    list_detail = rpm_line_id.detail_ids_2
+                elif rpm_line_id.is_detail3:
+                    model_detail = self.env['pwk.rpm.line.detail3']
+                    list_detail = rpm_line_id.detail_ids_3
+                elif rpm_line_id.is_detail4:
+                    model_detail = self.env['pwk.rpm.line.detail4']
+                    list_detail = rpm_line_id.detail_ids_4
+                elif rpm_line_id.is_detail5:
+                    model_detail = self.env['pwk.rpm.line.detail5']
+                    list_detail = rpm_line_id.detail_ids_5
+
+                if model_detail and list_detail:
+                    for bom in list_detail:
+                        model_detail.create({
+                            'reference': rpm_line_id.id,
+                            'product_id': bom.product_id.id,
+                            'thick': bom.thick,
+                            'width': bom.width,
+                            'length': bom.length,
+                            'ply': bom.ply,
+                            'quantity': bom.quantity,
+                        })
+
+
