1c78c5a1d69953469e9a80b52447d6055ad88659
Alfius Samuel <samuel.alfius@gmail.com>
2021-02-22 15:31:59 +0700

    Update rpm_excel.py

--- v12_pwk/models/rpm_excel.py
+++ v12_pwk/models/rpm_excel.py
@@ -82,7 +82,7 @@ class RpmReportXls(models.AbstractModel):
         formatHeaderDetailRight.set_text_wrap()
         formatHeaderDetailLeft.set_text_wrap()
 
-        formatHeaderDetailLeft.set_indent(1)
+        formatHeaderDetailLeft.set_indent(0.5)
         
         # Set Column Width
         sheet.set_column(0, 0, 3)
