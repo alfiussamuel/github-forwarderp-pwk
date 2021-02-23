3d7932799eec0bc2414727236484dcc2efc38647
Alfius Samuel <samuel.alfius@gmail.com>
2021-02-23 11:23:49 +0700

    Update rpm_monitoring_excel.py

--- v12_pwk/models/rpm_monitoring_excel.py
+++ v12_pwk/models/rpm_monitoring_excel.py
@@ -134,11 +134,12 @@ class RpmMonitoringReportXls(models.AbstractModel):
 
         date = lines.date_start
         column = 12
-        while lines.date_start < lines.date_end:
-            sheet.write(row+1, column, '', formatHeaderTable)
-            date = date + timedelta(days = 1)
-            print ("Date ", date)
-            column += 1
+        
+        # while lines.date_start < lines.date_end:
+        #     sheet.write(row+1, column, '', formatHeaderTable)
+        date = date + timedelta(days = 1)
+        print ("Date ", date)
+            # column += 1
 
         row = 7
         number = 1        
