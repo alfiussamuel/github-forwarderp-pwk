4f10627d8e09b1b451689e6c65f2fa238da33b9c
Alfius Samuel <samuel.alfius@gmail.com>
2021-02-24 14:01:03 +0700

    update

--- v12_pwk/wizard/generate_packing_list.py
+++ v12_pwk/wizard/generate_packing_list.py
@@ -6,30 +6,13 @@ from dateutil.parser import parse
 from odoo.exceptions import UserError
 from odoo import models, fields, api, _
 
-class PwkGeneratePackingListWizardLine(models.TransientModel):
-    _name = 'pwk.generate.packing.list.wizard.line'
-
-    reference = fields.Many2one('pwk.generate.packing.list.wizard', 'Reference')
-    sale_line_ids = fields.Many2many('sale.order.line', 'packing_list_wizard_line_sale_line_default_rel',
-        'packing_list_wizard_line_id', 'sale_line_id', string='Sales Order Lines')
-    total_product = fields.Float(compute="_get_total", string='Total Product')
-
-    @api.depends('sale_line_ids.product_id')
-    def _get_total(self):
-        for res in self:
-            total_product = 0
-
-            if res.sale_line_ids:
-                for line in res.sale_line_ids:
-                    total_product += 1
-
-            res.total_product = total_product
-
 
 class PwkGeneratePackingListWizard(models.TransientModel):
     _name = 'pwk.generate.packing.list.wizard'
 
     line_ids = fields.One2many('pwk.generate.packing.list.wizard.line', 'reference', string='List')
+    sale_line_ids = fields.Many2many('sale.order.line', 'packing_list_wizard_line_sale_line_default_rel',
+        'packing_list_wizard_line_id', 'sale_line_id', string='Sales Order Lines')
 
     @api.multi
     def button_generate(self):    	
@@ -37,8 +20,8 @@ class PwkGeneratePackingListWizard(models.TransientModel):
     	active_id = context.get('active_id', False)
     	packing_list_id = self.env['pwk.packing.list'].search([('id', '=', active_id)])
 
-    	if self.line_ids:
-            for line in self.line_ids:
+    	if self.sale_line_ids:
+            for line in self.sale_line_ids:
                 self.env['pwk.packing.list.line'].create({
                     'reference': packing_list_id.id,
                     'product_id': line.product_id.id,
