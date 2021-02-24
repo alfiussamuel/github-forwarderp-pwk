f3abc8ce200c3517e28f8bb6907e42ee369254f0
Alfius Samuel <samuel.alfius@gmail.com>
2021-02-24 10:21:44 +0700

    Update pwk_rpm.py

--- v12_pwk/models/pwk_rpm.py
+++ v12_pwk/models/pwk_rpm.py
@@ -409,7 +409,7 @@ class PwkRpmLine(models.Model):
 
 class PwkRpmBahanBaku(models.Model):    
     _name = "pwk.rpm.bahan.baku"
-    _order = 'goods_type desc,jenis_kayu_id asc,thick desc'
+    _order = 'goods_type desc,jenis_kayu_id asc,thick desc,width desc'
 
     reference = fields.Many2one('pwk.rpm', 'Reference')
     product_id = fields.Many2one('product.product', string='Product')
