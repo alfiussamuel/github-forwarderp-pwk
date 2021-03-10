e27102e0c462c244d3aafa0129e0011a7ab3e0cf
Alfius Samuel <samuel.alfius@gmail.com>
2021-03-10 13:55:32 +0700

    update

--- v12_pwk/models/pwk_packing_list.py
+++ v12_pwk/models/pwk_packing_list.py
@@ -45,6 +45,10 @@ class PwkPackingListLine(models.Model):
     sale_id = fields.Many2one('sale.order', 'No. Order')
     sale_line_id = fields.Many2one('sale.order.line', 'No. Order Line')
 
+    reference_partner_id = fields.Many2one(related="reference.partner_id", comodel_name='res.partner', string='Buyer')
+    reference_destination_id = fields.Many2one(related="reference.destination_id", comodel_name='pwk.destination', string='Destination')
+    reference_po_number = fields.Char(related="reference.po_number", string='Contract')
+
     crate_number = fields.Integer('Crate Number')
     crate_qty_each = fields.Integer('Crate Qty each')
 
