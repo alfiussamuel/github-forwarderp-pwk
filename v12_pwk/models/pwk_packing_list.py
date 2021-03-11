aead6c53a9f4130c7f961336d24f90c7fe97afc6
Alfius Samuel <samuel.alfius@gmail.com>
2021-03-11 15:05:17 +0700

    Update pwk_packing_list.py

--- v12_pwk/models/pwk_packing_list.py
+++ v12_pwk/models/pwk_packing_list.py
@@ -171,7 +171,7 @@ class PwkPackingListLine(models.Model):
 
             smaller_ids = self.env['pwk.packing.list.line'].search([
                 ('id', '<', res.id),
-                ('reference', '<', res.reference.id)
+                ('reference', '=', res.reference.id)
             ], order='id desc')
 
             if smaller_ids:
