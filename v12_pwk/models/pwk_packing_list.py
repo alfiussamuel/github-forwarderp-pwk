ad21b133a445925543ae5d0581d21cc5c5a07770
Alfius Samuel <samuel.alfius@gmail.com>
2021-03-31 08:21:59 +0700

    Update pwk_packing_list.py

--- v12_pwk/models/pwk_packing_list.py
+++ v12_pwk/models/pwk_packing_list.py
@@ -451,7 +451,10 @@ class PwkPackingList(models.Model):
                     'product_id': line.product_id.id,
                     'name': line.product_id.name,
                     'product_uom_qty': line.quantity,
-                    'product_uom': line.product_id.uom_id.id
+                    'product_uom': line.product_id.uom_id.id,
+                    'location_id': source_location_ids[0].id,
+                    'location_dest_id': destination_location_ids[0].id,
+                    'date': datetime.now()
                 })
 
             if picking_id:
