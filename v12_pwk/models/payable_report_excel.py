92a735e2fc6344b986a492f5af1c174411484f48
Alfius Samuel <samuel.alfius@gmail.com>
2021-06-30 10:27:29 +0700

    Update payable_report_excel.py

--- v12_pwk/models/payable_report_excel.py
+++ v12_pwk/models/payable_report_excel.py
@@ -49,6 +49,7 @@ class PayableReportXls(models.AbstractModel):
         formatHeaderCompany = workbook.add_format({'font_size': 12, 'align': 'left', 'bold': True})
         formatHeader = workbook.add_format({'font_size': 10, 'valign':'vcenter', 'align': 'left', 'bold': False, 'text_wrap': True})
         formatHeaderCenter = workbook.add_format({'font_size': 14, 'valign':'vcenter', 'align': 'center', 'bold': True, 'text_wrap': True})
+        formatHeaderLeft = workbook.add_format({'font_size': 14, 'valign':'vcenter', 'align': 'left', 'bold': True, 'text_wrap': True})
         formatHeaderRight = workbook.add_format({'font_size': 10, 'valign':'vcenter', 'align': 'left', 'num_format': '#,##0'})
         formatHeaderTable = workbook.add_format({'font_size': 10, 'valign':'vcenter', 'align': 'centre', 'bold': True, 'bg_color':'#4ead2f', 'color':'white', 'text_wrap': True})
         formatHeaderTableRight = workbook.add_format({'font_size': 10, 'valign':'vcenter', 'align': 'right', 'bold': True, 'bg_color':'#3eaec2', 'text_wrap': True, 'num_format': '#,##0'})
@@ -95,52 +96,46 @@ class PayableReportXls(models.AbstractModel):
         sheet.set_column(10, 10, 15)
         
         # Header        
-        sheet.merge_range(0, 0, 0, 4, 'PT PRIMA WANA KREASI INDUSTRY', formatHeaderCenter)
+        sheet.merge_range(0, 0, 0, 4, 'PT PRIMA WANA KREASI INDUSTRY', formatHeaderLeft)
+        sheet.merge_range(1, 0, 1, 4, 'REKAP HUTANG PRODUKSI', formatHeaderLeft)
         
         # Table Header
-        # sheet.merge_range(8, 0, 9, 0, 'NO', formatHeaderTable)
-        # sheet.merge_range(8, 1, 9, 1, 'NAMA', formatHeaderTable)
-        # sheet.merge_range(8, 2, 9, 2, 'NO. TDP', formatHeaderTable)
-        # sheet.merge_range(8, 3, 9, 3, 'ALAMAT', formatHeaderTable)
-        # sheet.merge_range(8, 4, 8, 6, 'Dokumen Pelengkap Ekspor', formatHeaderTable)
-        # sheet.merge_range(8, 7, 8, 10, 'Data Barang Ekspor Sesuai PEB', formatHeaderTable)
-        # sheet.merge_range(8, 11, 9, 11, 'BUYER', formatHeaderTable)
-        # sheet.merge_range(8, 12, 9, 12, 'LC / TT', formatHeaderTable)
-        # sheet.merge_range(8, 13, 9, 13, 'CNF / FOB', formatHeaderTable)
-        # sheet.merge_range(8, 14, 9, 14, 'Kurs BI', formatHeaderTable)
-        # sheet.merge_range(8, 15, 9, 15, 'Kurs MK', formatHeaderTable)
-        # sheet.merge_range(8, 16, 9, 16, 'TOTAL ( Kurs MK x Volume )', formatHeaderTable)
+        sheet.merge_range(2, 0, 2, 0, 'NO', formatHeaderTable)
+        sheet.merge_range(2, 1, 2, 1, 'Tanggal Penerimaan Invoice', formatHeaderTable)
+        sheet.merge_range(2, 2, 2, 2, 'Nama Supplier', formatHeaderTable)
+        sheet.merge_range(2, 3, 2, 3, 'No. Invoice', formatHeaderTable)
+        sheet.merge_range(2, 4, 2, 4, 'Tanggal Invoice', formatHeaderTable)
+        sheet.merge_range(2, 5, 2, 5, 'Jatuh Tempo', formatHeaderTable)
+        sheet.merge_range(2, 6, 2, 6, 'Outstanding (+/-)', formatHeaderTable)
+        sheet.merge_range(2, 7, 2, 7, 'Nilai Invoice', formatHeaderTable)
+        sheet.merge_range(2, 8, 2, 8, 'Nama Deskripsi Barang', formatHeaderTable)
+        sheet.merge_range(2, 9, 2, 9, 'Keterangan Invoice', formatHeaderTable)
+        sheet.merge_range(2, 10, 2, 10, 'Notes', formatHeaderTable)               
 
-        # sheet.write(9, 4, 'No. & Tgl. Dokumen V-Legal', formatHeaderTable)
-        # sheet.write(9, 5, 'No. & Tgl. PEB', formatHeaderTable)
-        # sheet.write(9, 6, 'Lokasi Stuffing', formatHeaderTable)
-        # sheet.write(9, 7, 'Volume ( M3 )', formatHeaderTable)
-        # sheet.write(9, 8, 'Netto ( Kg )', formatHeaderTable)
-        # sheet.write(9, 9, 'Jumlah ( Unit )', formatHeaderTable)
-        # sheet.write(9, 10, 'Nilai ( USD )', formatHeaderTable)                
+        row = 3
+        number = 1
+        for i in get_invoice:
+            'tanggal_penerimaan' : invoice.date_invoice,
+                'supplier' : invoice.partner_id.name,
+                'nomor_invoice' : invoice.number,
+                'tanggal_invoice' : invoice.date_invoice,
+                'tanggal_jatuh_tempo' : invoice.date_due,
+                'umur_jatuh_tempo' : due_days,
+                'nilai_invoice' : invoice.amount_total,
+                'deskripsi_barang' : '',
+                'keterangan' : '',
 
-        # row = 10
-        # number = 1
-        # for i in get_invoice:         
-        #     sheet.set_row(row, 55)
-        #     sheet.write(row, 0, number, formatHeaderDetailCenter)
-        #     sheet.write(row, 1, 'PT. PRIMA WANA KREASI WOOD INDUSTRY', formatHeaderDetailCenter)            
-        #     sheet.write(row, 2, i['tdp'], formatHeaderDetailCenter)
-        #     sheet.write(row, 3, alamat, formatHeaderDetailCenter)
-        #     sheet.write(row, 4, '', formatHeaderDetailCenter)
-        #     sheet.write(row, 5, '', formatHeaderDetailCenter)
-        #     sheet.write(row, 6, alamat, formatHeaderDetailCenter)
-        #     sheet.write(row, 7, i['volume'], formatHeaderDetailCenterNumberFour)
-        #     sheet.write(row, 8, i['netto'], formatHeaderDetailCenterNumber)
-        #     sheet.write(row, 9, i['jumlah'], formatHeaderDetailCenterNumber)
-        #     sheet.write(row, 10, i['nilai'], formatHeaderDetailCenterNumber)
-        #     sheet.write(row, 11, i['buyer'], formatHeaderDetailCenter)
-        #     sheet.write(row, 12, i['lc_tt'], formatHeaderDetailCenter)
-        #     sheet.write(row, 13, i['cnf_fob'], formatHeaderDetailCenter)
-        #     sheet.write(row, 14, i['kurs_bi'], formatHeaderDetailCenterNumber)
-        #     sheet.write(row, 15, i['kurs_mk'], formatHeaderDetailCenterNumber)
-        #     sheet.write(row, 16, i['total'], formatHeaderDetailCenterNumber)
-        #     # sheet.write_formula(row, 16, '{=SUM(B1:C1*B2:C2)}', cell_format, 2005)
-        #     row += 1
-        #     number += 1
+            sheet.write(row, 0, number, formatHeaderDetailCenter)
+            sheet.write(row, 1, i['tanggal_penerimaan'], formatHeaderDetailCenter)            
+            sheet.write(row, 2, i['supplier'], formatHeaderDetailCenter)
+            sheet.write(row, 3, i['nomor_invoice'], formatHeaderDetailCenter)
+            sheet.write(row, 4, i['tanggal_invoice'], formatHeaderDetailCenter)
+            sheet.write(row, 5, i['tanggal_jatuh_tempo'], formatHeaderDetailCenter)
+            sheet.write(row, 6, i['umur_jatuh_tempo'], formatHeaderDetailCenter)
+            sheet.write(row, 7, i['nilai_invoice'], formatHeaderDetailCenterNumberFour)
+            sheet.write(row, 8, i['deskripsi_barang'], formatHeaderDetailCenterNumber)
+            sheet.write(row, 9, i['keterangan'], formatHeaderDetailCenterNumber)
+            
+            row += 1
+            number += 1
 
